c9718499b5e42a3b5de9f992a057d920e1e6612d
praveenkumaarVLD <praveenkumaarvks2005@gmail.com>
2025-05-27 16:16:51 +0530

    Update testbench.v

--- Basic Gates/testbench.v
+++ Basic Gates/testbench.v
@@ -9,7 +9,7 @@ module basicgates_tb;
                 #10 a=1'b1;b=1'b1;
                 #50 $finish;
         end
-        always&(*)
+        always@(*)
                 begin
                         $display("Inputs:a=%b b%b ,Outputs:And=%b Or=%b Nand=%b Nor=%b Xor=%b Xnor=%b Not=%b ",a,b,out_and,out_or,out_nand,out_nor,out_xor,out_xnor,out_not);
                 end
